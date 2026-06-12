b996bb96eefc041905c05bbf3c6096a923caeb5f
Vasco de Krijger <akatchi@codekrijger.io>
2026-06-12 08:33:48 +0200

    feat: call applyAppearance at app startup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 import { initAnalytics } from './lib/analytics/analytics'
+import { applyAppearance } from './lib/settings/appearance.svelte'
 
 initAnalytics()
+applyAppearance()
 mount(App, { target: document.getElementById('app')! })
